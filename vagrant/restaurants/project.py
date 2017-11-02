654a56b9de99866170c2836a32762e08658fe68c
Anthony RTK <sokasoka@hotmail.com>
2017-11-01 19:18:05 -0700

    working on lesson 3

---
+++ vagrant/restaurants/project.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurants/<int:restaurant_id>/menu')
+def restaurant_menu(restaurant_id):
+  restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+  items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+  return render_template('menu.html', restaurant=restaurant, items=items)
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+  restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+
+  if request.method == 'POST':
+    new_item = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+    session.add(new_item)
+    session.commit()
+    return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
+  else:
+    return render(url_for('newmenuitem.html', restaurant=restaurant))
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+  return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+  return "page to delete a menu item. Task 3 complete!"
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host='0.0.0.0', port=5000)
